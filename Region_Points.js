2d04c5f91d19d319d199aa92eeb820e796d1d6c1
MarinaLazaridou <54821606+MarinaLazaridou@users.noreply.github.com>
2020-03-10 17:02:30 +0000

    Add files via upload

---
+++ Region_Points.js
@@ -0,0 +1,93 @@
+var regionPoints = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {name: 'South West and the Channel Islands'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.6477050781249996,
+          51.020666012558095
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: 'Wales'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -3.7683105468749996,
+          52.02545860348814
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: 'South East'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1483154296875,
+          51.034485632974125
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: 'London'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1318359375,
+          51.51216124955515
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: 'Midlands'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -1.0574340820312498,
+          52.63889682030036
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: 'North England'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -1.93084716796875,
+          54.401346982784176
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: 'Scotland'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -3.812255859375,
+          56.81892067431724
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: 'Northern Ireland and the Isle of Man'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -6.6357421875,
+          54.81967870427068
+        ]
+      }
+    }
+  ]
+}
